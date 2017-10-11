02e798f3a9af41fc63eb38c7948d377a4895b705
Hubmeat_tl <satantanqiuqiu@163.com>
2017-10-11 16:44:43 +0800

    1

--- src/router/index.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import routeConfig from './router-config.js'
-
-Vue.use(Router)
-export default new Router({
-  routes: routeConfig,
-  mode: 'hash'
-})
