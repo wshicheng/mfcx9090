271501dec80883f1611a29fe5f6b04882030b4d3
Hubmeat_tl <satantanqiuqiu@163.com>
2017-10-11 18:41:40 +0800

    1

--- src/config/index.js
+++ src/config/index.js
@@ -5,7 +5,7 @@
 // export const host = 'http://192.168.0.106:8080/'
 // export const host = 'http://192.168.0.128:8080/'
 // export const host = 'http://192.168.0.120:8081/'
-export const host = 'http://192.168.0.103:8080/'
+// export const host = 'http://192.168.0.103:8080/'
 // export const host = 'http://192.168.0.120:8080/'
 
 /**
@@ -26,7 +26,7 @@ export const host = 'http://192.168.0.103:8080/'
  */
 // export const host  = 'http://47.92.136.117:7099/'
 // export const host = 'http://47.93.48.250:8090/'
-// export const host = 'http://47.93.48.250:8090/'
+export const host = 'http://47.93.48.250:8090/'
 // export const host = 'http://47.93.48.250:8090/'
 // export const host = 'http://192.168.3.233:8080/'
 
